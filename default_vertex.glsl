d61692ce27245bfc78c0be36be1a210cf8ebaa9c
CobaltXII <sidatt64@gmail.com>
2019-01-12 20:47:12 -0500

    Added specularity in/out

--- default_vertex.glsl
+++ default_vertex.glsl
@@ -8,6 +8,8 @@ layout (location = 1) in vec3 attribute_color;
 
 layout (location = 2) in vec3 attribute_normal;
 
+layout (location = 3) in float attribute_specular;
+
 // Input matrices.
 
 uniform mat4 matrix_projection;
@@ -24,6 +26,8 @@ out vec3 color_attribute;
 
 out vec3 normal_attribute;
 
+out float specular_attribute;
+
 out vec3 light;
 
 // Main shader code.
@@ -37,14 +41,16 @@ void main()
 
 	gl_Position = matrix_projection * matrix_view * matrix_model * vec4(attribute_position, 1.0f);
 
-	// Pass the position attribute, color attribute and normal attribute to 
-	// the fragment shader.
+	// Pass the position attribute, color attribute, normal attribute and 
+	// specular attribute to the fragment shader.
 
 	position_attribute = attribute_position;
 
 	color_attribute = attribute_color;
 
 	normal_attribute = attribute_normal;
+
+	specular_attribute = attribute_specular;
 
 	// Rotate the light's direction to ensure that the light is always facing
 	// from the camera towards the planet. Pass the light direction to the 
