7c10dbb648110b4ccfc22bf2ac01e4507c2a6d00
CobaltXII <sidatt64@gmail.com>
2019-01-12 19:01:45 -0500

    Implemented std::vector<glm::vec3> create_icosphere()

--- planet.cpp
+++ planet.cpp
@@ -101,3 +101,112 @@ int get_middle_point(std::vector<glm::vec3>& vector, int p_1, int p_2)
 
 	return i;
 }
+
+/*
+
+Create an icosphere with the given amount of subdivisions.
+
+*/
+
+std::vector<glm::vec3> create_icosphere(int subdivisions = 8)
+{ 
+	// Generate the icosphere's vertices.
+
+	std::vector<glm::vec3> icosphere_vertices;
+
+	// Generate the 12 vertices of an icosahedron.
+
+	float t = (1.0f + sqrt(5.0f)) / 2.0f;
+
+	add_vertex(icosphere_vertices, glm::vec3(-1.0f,  t, 0.0f));
+	add_vertex(icosphere_vertices, glm::vec3( 1.0f,  t, 0.0f));
+	add_vertex(icosphere_vertices, glm::vec3(-1.0f, -t, 0.0f));
+	add_vertex(icosphere_vertices, glm::vec3( 1.0f, -t, 0.0f));
+
+	add_vertex(icosphere_vertices, glm::vec3(0.0f, -1.0f,  t));
+	add_vertex(icosphere_vertices, glm::vec3(0.0f,  1.0f,  t));
+	add_vertex(icosphere_vertices, glm::vec3(0.0f, -1.0f, -t));
+	add_vertex(icosphere_vertices, glm::vec3(0.0f,  1.0f, -t));
+
+	add_vertex(icosphere_vertices, glm::vec3( t, 0.0f, -1.0f));
+	add_vertex(icosphere_vertices, glm::vec3( t, 0.0f,  1.0f));
+	add_vertex(icosphere_vertices, glm::vec3(-t, 0.0f, -1.0f));
+	add_vertex(icosphere_vertices, glm::vec3(-t, 0.0f,  1.0f));
+
+	// Generate the 20 faces of an icosahedron.
+
+	std::vector<triangle_indices> icosphere_indices;
+
+	icosphere_indices.push_back(triangle_indices(0x0, 0xB, 0x5));
+	icosphere_indices.push_back(triangle_indices(0x0, 0x5, 0x1));
+	icosphere_indices.push_back(triangle_indices(0x0, 0x1, 0x7));
+	icosphere_indices.push_back(triangle_indices(0x0, 0x7, 0xA));
+	icosphere_indices.push_back(triangle_indices(0x0, 0xA, 0xB));
+
+	icosphere_indices.push_back(triangle_indices(0x1, 0x5, 0x9));
+	icosphere_indices.push_back(triangle_indices(0x5, 0xB, 0x4));
+	icosphere_indices.push_back(triangle_indices(0xB, 0xA, 0x2));
+	icosphere_indices.push_back(triangle_indices(0xA, 0x7, 0x6));
+	icosphere_indices.push_back(triangle_indices(0x7, 0x1, 0x8));
+
+	icosphere_indices.push_back(triangle_indices(0x3, 0x9, 0x4));
+	icosphere_indices.push_back(triangle_indices(0x3, 0x4, 0x2));
+	icosphere_indices.push_back(triangle_indices(0x3, 0x2, 0x6));
+	icosphere_indices.push_back(triangle_indices(0x3, 0x6, 0x8));
+	icosphere_indices.push_back(triangle_indices(0x3, 0x8, 0x9));
+
+	icosphere_indices.push_back(triangle_indices(0x4, 0x9, 0x5));
+	icosphere_indices.push_back(triangle_indices(0x2, 0x4, 0xB));
+	icosphere_indices.push_back(triangle_indices(0x6, 0x2, 0xA));
+	icosphere_indices.push_back(triangle_indices(0x8, 0x6, 0x7));
+	icosphere_indices.push_back(triangle_indices(0x9, 0x8, 0x1));
+
+	// Subdivide the icosphere.
+
+	for (int i = 0; i < subdivisions; i++)
+	{
+		// Generate a temporary mesh to hold the result of the next 
+		// subdivision.
+
+		std::vector<triangle_indices> new_icosphere_indices;
+
+		// Subdivide each triangle in the current mesh.
+
+		for (int j = 0; j < icosphere_indices.size(); j++)
+		{
+			triangle_indices tri = icosphere_indices[j];
+
+			int a = get_middle_point(icosphere_vertices, std::get<0>(tri), std::get<1>(tri));
+			int b = get_middle_point(icosphere_vertices, std::get<1>(tri), std::get<2>(tri));
+			int c = get_middle_point(icosphere_vertices, std::get<2>(tri), std::get<0>(tri));
+
+			// Add the 4 new triangles to the temporary mesh.
+
+			new_icosphere_indices.push_back(triangle_indices(std::get<0>(tri), a, c));
+			new_icosphere_indices.push_back(triangle_indices(std::get<1>(tri), b, a));
+			new_icosphere_indices.push_back(triangle_indices(std::get<2>(tri), c, b));
+
+			new_icosphere_indices.push_back(triangle_indices(a, b, c));
+		}
+
+		// Replace the current mesh with the temporary mesh.
+
+		icosphere_indices = new_icosphere_indices;
+	}
+
+	// Convert the icosphere's structured triangle_indices vector to a list of
+	// ordered vertices.
+
+	std::vector<glm::vec3> icosphere_mesh;
+
+	for (int i = 0; i < icosphere_indices.size(); i++)
+	{
+		icosphere_mesh.push_back(icosphere_vertices[std::get<0>(icosphere_indices[i])]);
+		icosphere_mesh.push_back(icosphere_vertices[std::get<1>(icosphere_indices[i])]);
+		icosphere_mesh.push_back(icosphere_vertices[std::get<2>(icosphere_indices[i])]);
+	}
+
+	// Return the icosphere's mesh.
+
+	return icosphere_mesh;
+};
